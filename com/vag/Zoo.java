398997ce24163bb1555a5ba36a6406a1828a8aa7
vageesh <vageeshs7@gmail.com>
2017-07-11 17:03:59 +0530

    initializer blocks

--- com/vag/Zoo.java
+++ com/vag/Zoo.java
@@ -1,9 +1,36 @@
 package com.vag;
 
 public class Zoo{
+	private String name;
+	private int ID = 0;
+	public Zoo(String name)
+	{
+		this.name = name;
+		//this.ID = this.name.length();
+		System.out.println("constructor");
+	}
+
+	{
+		ID = -1;
+	}
+	
+	public String toString()
+	{
+		return ID + "-" + name;
+	}
+
+	static {
+		System.out.println("initializer class level - static");
+	}	
+	
 	public static void main(String... args){
-		System.out.println("Hello world - " + args);
-		for(int i=0; i< args.length; i++)
-			System.out.println(args[i]);	
+		
+		System.out.println("Hello world - ");
+		for(int i=0; i< args.length; i++){
+			Zoo obj = new Zoo(args[i]);
+			System.out.println(obj);
+		}
+		
+					
 	}
 }
